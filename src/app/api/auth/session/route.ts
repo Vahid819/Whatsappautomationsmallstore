f345cd55634a562696929b1cc0e0c12b6bcd9b09
Vahid Momin <vahidmomin.dev@gmail.com>
2026-08-14 20:46:04 +0530

    fix the middleware

--- src/app/api/auth/session/route.ts
+++ src/app/api/auth/session/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-import { adminAuth } from "@/lib/firebase/admin";
+import { adminAuth } from "@/lib/firebase/admin-auth";
 
 export async function POST(
   request: NextRequest
